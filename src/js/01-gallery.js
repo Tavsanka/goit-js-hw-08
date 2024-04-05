51cdcaa0503229e4440decbd9fea349fad8cdf9b
Tavsanka <domnikan@gmail.com>
2024-04-05 21:59:22 +0200

    updated gallery1 js

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,25 @@
 // Add imports above this line
 import { galleryItems } from './gallery-items';
 // Change code below this line
+import SimpleLightbox from 'simplelightbox';
+import { galleryItems } from './gallery-items.js';
+import 'simplelightbox/dist/simple-lightbox.css';
+// Change code below this line
+const gallery = document.querySelector('ul.gallery');
+for (const item of galleryItems) {
+  const html = `<li class="gallery__item">
+    <a class="gallery__link" href="${item.original}">
+      <img
+        class="gallery__image"
+        src="${item.preview}"
+        data-source="${item.original}"
+        alt="${item.description}"
+      />
+    </a>
+  </li>`;
+  gallery.insertAdjacentHTML('beforeend', html);
+}
+
+const lightbox = new SimpleLightbox('.gallery a');
 
 console.log(galleryItems);
